5027f8a5ce5a1102655d349b4b3f9a00e8f7c51e
Flow-Pie <stratabase@gmail.com>
2025-03-23 21:25:42 +0300

    UPDATE:  improved candidate profile viewing

--- OnlineVotingSystem.api/obj/Debug/net8.0/OnlineVotingSystem.api.AssemblyInfo.cs
+++ OnlineVotingSystem.api/obj/Debug/net8.0/OnlineVotingSystem.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineVotingSystem.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ad27a5a534dbc010398bfa63ae548ec758cf385")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+749e927940c4ed96a72cf8c17b5e581b88b8a2ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineVotingSystem.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineVotingSystem.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
+++ WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ad27a5a534dbc010398bfa63ae548ec758cf385")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+749e927940c4ed96a72cf8c17b5e581b88b8a2ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
