fd162219c62ac9adb3b4fc5ab5ad65e8886b1aa0
Flow-Pie <stratabase@gmail.com>
2025-03-25 23:24:10 +0300

    CLEANUP: applied monochromatic color scheme with 60/30/10 rule

--- OnlineVotingSystem.api/obj/Debug/net8.0/OnlineVotingSystem.api.AssemblyInfo.cs
+++ OnlineVotingSystem.api/obj/Debug/net8.0/OnlineVotingSystem.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineVotingSystem.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd69317e2feae83ab82d0c4e91a7e8a8ef67ae84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab5ceacc273fe819c8ff1dc72a02a39c518f0550")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineVotingSystem.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineVotingSystem.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
